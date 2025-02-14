5564f9bd1d8a86ecf654a7c090a42ba38ebb1f25
George Silva <georgeosabio@gmail.com>
2025-02-13 21:04:14 -0400

    primeiro commit

---
+++ main.py
@@ -0,0 +1,19 @@
+print("jornada de dados")
+
+# variáveis
+idade = 47
+nome = "George"
+
+# print
+print(idade)
+
+# Soma
+new_age = idade + 1
+print(new_age)
+
+# lista
+lista = [1,2,3,4,5]
+
+# Métodos
+lista_inventario = lista.reverse()
+
