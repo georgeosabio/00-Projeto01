ec9482f6e8acdda034ae8259dce8c869ba454f53
George Silva <georgeosabio@gmail.com>
2025-02-27 20:49:10 -0400

    Update aula 01

--- exemplo_01.py
+++ exemplo_01.py
@@ -1,1 +1,9 @@
-print("Alguma coisa")
+# print("Alguma coisa")
+
+# print("Olá, " + input("Digite seu nome: ") + "!")
+
+# print(len(input("Digite seu nome: ")))
+
+nome = input("Digite seu nome: ")
+qtd_nome = len(nome)
+print(qtd_nome)
